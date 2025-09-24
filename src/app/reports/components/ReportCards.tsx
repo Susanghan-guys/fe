d88e573ebc75459730174bdc6f253367432fd85c
chasyuss <cotn963@gmail.com>
2025-09-24 23:26:29 +0900

    [SSN-174] feat: 모바일 delete아이콘 추가

--- src/app/reports/components/ReportCards.tsx
+++ src/app/reports/components/ReportCards.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import CheckBox from "../../../../public/icons/CheckBox";
 import { ReportDelete } from "../../../../public";
 import { useIsMobile } from "@/hooks/useIsMobile";
 import DeleteConfirmModal from "./DeleteConfirmModal";
@@ -60,40 +59,52 @@ const ReportCard = ({
 
   if (isMobile) {
     return (
-      <div className="rounded-[12px] bg-white p-5 w-full mb-6">
-        <div className="flex flex-col">
-          <div className="flex flex-row justify-between w-full">
-            <div className="py-1 px-2 rounded-[4px] font-C01-M flex justify-center items-center bg-blue-50 text-blue-main">
-              {type}
+      <>
+        <div className="rounded-[12px] bg-white p-5 w-full mb-6">
+          <div className="flex flex-col">
+            <div className="flex flex-row justify-between w-full">
+              <div className="py-1 px-2 rounded-[4px] font-C01-M flex justify-center items-center bg-blue-50 text-blue-main">
+                {type}
+              </div>
+
+              <div className="ml-auto" onClick={handleDeleteClick}>
+                <ReportDelete />
+              </div>
             </div>
-            <div className="flex flex-row gap-2 items-center">
+            <div className="flex flex-row justify-between items-center w-full mt-5">
+              <div className="text-gray-900 font-B02-SB">{title}</div>
               <div
                 className={`font-C01-M ${
-                  isCompleted ? "text-gray-800" : "text-gray-500"
+                  isCompleted ? "text-blue-main" : "text-gray-500"
                 }`}
               >
                 {status}
               </div>
-              {isCompleted && <CheckBox />}
             </div>
+            <div className="text-gray-500 font-C01-R mt-[4px]">{org}</div>
+            <div className="flex flex-row mt-[32px]">
+              <div className="text-gray-900 font-C01-M">참여자</div>
+              <div className="w-[1px] h-4 bg-gray-200 mx-2 mt-[2px]"></div>
+              <div className="text-gray-700 font-C01-R">{participants}</div>
+            </div>
+            {isCompleted && (
+              <button
+                onClick={navigateToReport}
+                className="flex mt-[22px] items-center justify-center px-[22px] py-3 rounded-[10px] bg-blue-main cursor-pointer"
+              >
+                <div className="text-white font-B03-M">리포트 보기</div>
+              </button>
+            )}
           </div>
-          <div className="text-gray-900 font-B02-SB mt-[16px]">{title}</div>
-          <div className="text-gray-500 font-C01-R mt-[4px]">{org}</div>
-          <div className="flex flex-row mt-[32px]">
-            <div className="text-gray-900 font-C01-M">참여자</div>
-            <div className="w-[1px] h-4 bg-gray-200 mx-2 mt-[2px]"></div>
-            <div className="text-gray-700 font-C01-R">{participants}</div>
-          </div>
-          {isCompleted && (
-            <button
-              onClick={navigateToReport}
-              className="flex mt-[22px] items-center justify-center px-[22px] py-3 rounded-[10px] bg-blue-main cursor-pointer"
-            >
-              <div className="text-white font-B03-M">리포트 보기</div>
-            </button>
-          )}
         </div>
-      </div>
+        <DeleteConfirmModal
+          isOpen={isDeleteModalOpen}
+          onClose={() => setIsDeleteModalOpen(false)}
+          onConfirm={handleDeleteConfirm}
+          title={title}
+          type={modalType}
+        />
+      </>
     );
   }
 
